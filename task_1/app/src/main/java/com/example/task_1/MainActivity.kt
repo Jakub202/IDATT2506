161409fb467cdea7da0a4ca51eaaeb9c1334f2e7
Jakub <jakubmr@stud.ntnu.no>
2023-09-03 21:40:34 +0200

    finished task1

---
+++ task_1/app/src/main/java/com/example/task_1/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.task_1
+
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import com.example.task_1.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menu.add("Valg 1")
+        menu.add("Valg 2")
+        Log.d("INFT2501", "Menu created")
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.title == "Valg 1") {
+            Log.w("INFT2501", "Valg 1 selected")
+        }
+        if (item.title == "Valg 2") {
+            Log.e("INFT2501", "Valg 2 selected")
+        }
+        return true
+    }
+}
