123c71016b3cac173b4824365cace598199cb18e
Jakub <jakubmr@stud.ntnu.no>
2023-11-27 18:24:42 +0100

    added red color for ineditable cells

--- sudoku/components/Cell.js
+++ sudoku/components/Cell.js
@@ -4,15 +4,19 @@ import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 const Cell = ({ number, isEditable, isMarked, isSelected, onSelect, style }) => {
     const cellStyle = [
         styles.cell,
-        isEditable && styles.editableCell,
+        isEditable ? styles.editableCell : styles.nonEditableCell,
         isMarked && styles.markedCell,
         isSelected && styles.selectedCell,
         style, // Apply additional styles passed as a prop
     ];
+    const textStyle = [
+        styles.cellText,
+        !isEditable && styles.boldText, // Apply bold style if not editable
+    ];
 
     return (
         <TouchableOpacity style={cellStyle} onPress={onSelect}>
-            <Text style={styles.cellText}>{number}</Text>
+            <Text style={textStyle}>{number}</Text>
         </TouchableOpacity>
     );
 };
@@ -28,6 +32,13 @@ const styles = StyleSheet.create({
     },
     editableCell: {
         backgroundColor: '#fff',
+    },
+    nonEditableCell: {
+        backgroundColor: '#e9e9e9', // You can choose a different color to indicate non-editable cells
+    },
+    boldText: {
+        fontWeight: 'bold',
+        color: 'red'// Bold font weight for non-editable numbers
     },
     markedCell: {
         backgroundColor: 'lightblue',
